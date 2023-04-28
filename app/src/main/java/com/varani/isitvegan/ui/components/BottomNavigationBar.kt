849c8cb7137c6aeba5dbd99dc7056f0569c18b1e
Ana Varani <apvarani@gmail.com>
2023-04-28 15:21:22 +0100

    Update NavigationBar colour

--- app/src/main/java/com/varani/isitvegan/ui/components/BottomNavigationBar.kt
+++ app/src/main/java/com/varani/isitvegan/ui/components/BottomNavigationBar.kt
@@ -1,7 +1,10 @@
 package com.varani.isitvegan.ui.components
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,7 +29,6 @@ fun BottomNavigationBar(
 ) {
     NavigationBar(
         modifier = modifier,
-        containerColor = MaterialTheme.colorScheme.primary,
         tonalElevation = 5.dp
     ) {
         items.forEach { item ->
